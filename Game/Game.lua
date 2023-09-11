58e553fdc366515e9584bce7d6379911a9b514b6
Crypto Logiq <orbitaldigital@gmail.com>
2023-09-11 22:32:40 +0200

    Scene Game Minimal code : work

---
+++ Game/Game.lua
@@ -0,0 +1,26 @@
+local Game = {debug=true}
+
+function Game.load()
+end
+--
+
+function Game.update(dt)
+end
+--
+
+function Game.draw()
+  if Game.debug then
+    love.graphics.print("Scene Game",10,10)
+  end
+end
+--
+
+function Game.keypressed(k)
+end
+--
+
+function Game.mousepressed(x,y,button)
+end
+--
+
+return Game
